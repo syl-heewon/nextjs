451580df89e3cbdafb72e797904da6a5b97efae6
heewon <heewon@heewonui-MacBookPro.local>
2023-02-24 14:56:10 +0900

    Add test message

--- pages/index.js
+++ pages/index.js
@@ -17,7 +17,7 @@ export default function Home() {
       <main className={styles.main}>
         <div className={styles.description}>
           <p>
-            Get started by editing&nbsp;
+            테스트 페이지!!&nbsp;
             <code className={styles.code}>pages/index.js</code>
           </p>
           <div>
